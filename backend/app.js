83c9843e33e51d6ec7c0d7ca52f5fc6a76e7ba8f
jeevaphysicist <jeevaphysicist@gmail.com>
2022-10-20 13:16:37 +0530

    submission

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = 8080 ;
 const app = express();
 
+
+// middle wares
+
+app.use(bodyParser.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("hi this get method !!!");
+});
+
+app.get('/hi',function(req,res){
+    res.send("hi this get method  2!!!");
+});
+app.post('/post',function(req,res){
+          console.log(req.body);
+     
+    res.send("hi this this is post method  !!!");
+});
+app.post('/post2',function(req,res){
+    console.log(req.body);
+
+res.send("hi this this is post method  2 !!!");
+});
 
 
 
